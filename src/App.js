73989f3e962a7090e96d0073edf8a60fb04bebca
John Walker <anni@phinix.X.corp>
2024-07-28 17:16:06 +0530

    I used rooter to navigate between pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
+import 'bootstrap';
 import Navbar from './Components/Navbar.js';
-import Textb from './Components/Textb.js';
+import { Textb } from './Components/Textb.js';
+import About from './Components/About.js';
 import './Components/Custom.css';
 import { useState } from 'react';
-// import Car
+import { BrowserRouter as Router, Route , Routes} from 'react-router-dom';
+
 function App() {
   const [mode, setMode] = useState('light');
   const toggle = () => {
-     if(mode==='light'){
+    if (mode === 'light') {
       setMode('dark');
-     }
-     else{
+    }
+    else {
       setMode('light');
-     }
+    }
   }
   return (
     <div className={`${mode}-blue-body`}>
-      <Navbar title={"TEXT-UTILS"} mod={mode} Toggle={toggle}/>
-      <Textb mod={mode}/>
+      <Router>
+        <Navbar title={"TEXT-UTILS"} mod={mode} Toggle={toggle} />
+        <Routes>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/' element={<Textb mod={mode} toggle={toggle}/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,12 +3,15 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './Custom.css';
+import { Link } from 'react-router-dom';
+
 
 function NavScrollExample(props) {
   return (
     <Navbar expand="lg" className={`navbar ${props.mod}-blue`} >
       <Container fluid>
         <Navbar.Brand href="#">{props.title}</Navbar.Brand>
+        
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -16,19 +19,10 @@ function NavScrollExample(props) {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link  to="#action2">Contact</Link>
           </Nav>
-          {/* <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form> */}
           <Form.Check // prettier-ignore
             onClick={props.Toggle}
             type="switch"
